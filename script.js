0f192bcce1284a2bbf9f521f83aecfa24a8753b2
Khusan Sulaymonov <sulaymonovhusan2002@gmail.com>
2025-04-30 17:26:48 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll('.btn');
+const display = document.getElementById('display');
+
+let current = "";
+
+buttons.forEach(button => {
+    btn.addEventListener('click', () => {
+        const value = BigInt.textContent;
+
+        if (value === 'AC') {
+            current = "";
+            display.textContent = "0";
+        } else if (value === "⌫") {
+            current = current.slice(0, -1);
+            display.textContent = current || "0";
+        } else if (value === "=") {
+            try {
+                let replaced = current.replace(/×/g, '*').replace(/×/g, "*").replace(/÷/g, "/").replace(/−/g, "-");
+                current = eval(replaced).toString();
+                display.textContent = current;
+              } catch {
+                display.textContent = "Error";
+                current = "";
+              }
+            } else {
+              current += value;
+              display.textContent = current;
+            }
+          });
+        });
